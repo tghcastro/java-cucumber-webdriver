78d30c762c24f4be5dd1811dd501f515d171d240
Thiago de Castro <tghcastro@gmail.com>
2023-04-24 17:29:56 +0100

    searching for content implementation

--- src/test/java/tghcastro/pageobjects/SearchResultsPage.java
+++ src/test/java/tghcastro/pageobjects/SearchResultsPage.java
@@ -1,14 +1,38 @@
 package tghcastro.pageobjects;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import tghcastro.pageobjects.components.SearchQueryCorrection;
 import tghcastro.pageobjects.support.InstancesRepository;
 
+import java.time.Duration;
+
 public class SearchResultsPage {
+    private final WebDriver driver;
+
+    public SearchResultsPage() {
+        this.driver = InstancesRepository.getUIAutomationDriver();
+    }
+
     public SearchQueryCorrection searchQueryCorrection() {
         return InstancesRepository.loadInstance(SearchQueryCorrection.class);
     }
 
     public String displayedUser() {
         return "Thiago de Castro";
+    }
+
+    public void shouldBeOpen(String queryValue) {
+        WebDriverWait wait = new WebDriverWait(this.driver, Duration.ofSeconds(5));
+
+        wait.until(ExpectedConditions.titleIs(queryValue + " - YouTube"));
+        wait.until(ExpectedConditions.urlContains("search_query=" + queryValue));
+
+        String aboutResultsXpath = "//span[contains(text(),'About these results')]";
+        WebElement aboutResults = this.driver.findElement(By.xpath(aboutResultsXpath));
+        wait.until(ExpectedConditions.visibilityOf(aboutResults));
     }
 }
--- src/test/java/tghcastro/pageobjects/components/SearchBar.java
+++ src/test/java/tghcastro/pageobjects/components/SearchBar.java
@@ -1,12 +1,49 @@
 package tghcastro.pageobjects.components;
 
+import org.openqa.selenium.By;
+import org.openqa.selenium.Keys;
+import org.openqa.selenium.WebDriver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.ui.ExpectedConditions;
+import org.openqa.selenium.support.ui.WebDriverWait;
 import tghcastro.pageobjects.SearchResultsPage;
+import tghcastro.pageobjects.support.InstancesRepository;
+
+import java.time.Duration;
 
 public class SearchBar {
+    private final WebDriver driver;
+    private WebElement searchField;
+    private WebElement searchInput;
 
-    public SearchResultsPage searchFor(String userName) {
+    public SearchBar() {
+        this.driver = InstancesRepository.getUIAutomationDriver();
+    }
+
+    public void searchFor(String userName) {
         System.out.println("Searching for " + userName);
-        return new SearchResultsPage();
+
+        WebDriverWait wait = new WebDriverWait(this.driver, Duration.ofSeconds(5));
+
+        String xpath = "//*[@id=\"search-input\"]";
+        this.searchField = this.driver.findElement(By.xpath(xpath));
+        wait.until(ExpectedConditions.visibilityOf(this.searchField));
+        this.searchField.click();
+
+        System.out.println("Search field is available");
+
+        this.searchInput = this.driver.findElement(By.name("search_query"));
+        this.searchInput.sendKeys(userName);
+        this.searchInput.sendKeys(Keys.RETURN);
+
+        SearchResultsPage results = InstancesRepository.loadInstance(SearchResultsPage.class);
+        results.shouldBeOpen(userName);
+
+        try {
+            Thread.sleep(5000);
+        } catch (InterruptedException e) {
+            throw new RuntimeException(e);
+        }
     }
 
 }
